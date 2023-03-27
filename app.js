80bef5ef6913e6f92e05dabe1c0ef682c0b83d99
Upshur06 <mupshur2005@gmail.com>
2023-03-27 14:10:41 -0400

    inital start setup app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    console.log('This is the "/" root page');
+    res.render('home');
+})
 
 app.listen(port, ()=>{
     console.log(`listening to port: ${port}`);
