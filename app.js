bfa9c5e7f5357d19ea2754d75b07f482c8cdde5a
Upshur06 <mupshur2005@gmail.com>
2023-03-29 22:06:44 -0400

    edit and delete edibles

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Edible = require("./models/edibleDelights");
 
 mongoose.connect('mongodb://localhost:27017/edibleDelights', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-// .then(() =>{
-//     console.log("CONNECTION OPEN");
-// })
-// .catch((error) => {
-//     console.log('NO error');
-//     console.log(error)
-// });
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -26,7 +20,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log('This is the "/" root page');
@@ -55,16 +51,34 @@ app.get('/createtreat/new', (req, res) => {
 })
 
 app.post('/edibletreats', async (req, res) => {
-    const treat = new Edible(req.body.edible);
+    // console.log(req.body);
+    const treat = new Edible(req.body);
     await treat.save();
-    res.redirect(`/edibletreats/${treat._id}`);
+    res.redirect(`/edibletreat/${treat._id}`);
 })
 
-app.get('/edibletreats/:id', async (req, res) => {
+
+app.get('/edibletreat/:id', async (req, res) => {
     const treat = await Edible.findById(req.params.id);
     res.render('treats/detail', {treat});
 })
 
+app.get('/edibletreat/:id/edit', async (req, res) => {
+    const treat = await Edible.findById(req.params.id);
+    res.render('treats/edit', {treat});
+})
+
+app.put('/edibletreat/:id', async (req, res) => {
+    const { id } = req.params;
+    const treat = await Edible.findByIdAndUpdate(id,{ ...req.body });
+    res.redirect(`/edibletreat/${treat._id}`);
+})
+
+app.delete('/edibletreat/:id', async (req, res) => {
+    const { id } = req.params;
+    await Edible.findByIdAndDelete(id);
+    res.redirect(`/edibletreats`);
+})
 
 app.listen(3000, ()=>{
     console.log(`listening to port: 3000`);
